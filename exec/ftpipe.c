22d70deb2b16eeb52c5c75d37f7e79c6a5bac20e
Anas TABITI <tabiti.anas@ofppt-edu.ma>
2022-05-31 13:54:31 +0100

    ad

--- exec/ftpipe.c
+++ exec/ftpipe.c
@@ -6,14 +6,13 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 08:51:00 by atabiti           #+#    #+#             */
-/*   Updated: 2022/05/31 11:50:28 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/05/31 13:52:49 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 int ft_pipe(struct s_list *list)
-
 {
 
 	if (list->there_is_pipe > 0)
@@ -29,16 +28,21 @@ int ft_pipe(struct s_list *list)
 			pipe(fd);
 
 			id = fork();
+			// if (i == 0)
+			// {
+			// 	ft_is_built_in(list);
+			// 	i++;
+			// }
 			if (id == 0)
 			{
 				dup2(fd_in, 0);
 				if (i < list->there_is_pipe)
-				
-					dup2(fd[1], 1);
-			
+
+				 	dup2(fd[1], 1);
+
 				close(fd[0]);
 
-				if (execve(list[1].cmd[0], list[1].cmd, NULL) == -1)
+				if (execve(list[i].cmd[0], list[i].cmd, NULL) == -1)
 				{
 					printf("failed\n");
 					return 0;
@@ -48,8 +52,9 @@ int ft_pipe(struct s_list *list)
 			else
 			{
 				list->fd_out = fd[1];
-				ft_is_built_in(list);
 				close(fd[1]);
+				// close(fd[0]);
+
 				wait(NULL);
 
 				fd_in = fd[0];
--- exec/main.c
+++ exec/main.c
@@ -31,8 +31,9 @@ int main(int ac, char **av, char **env)
 		//
 
 		ft_init(list);
-		list[0].cmd[0] = "/usr/bin/wc";
-		list[0].cmd[1] = NULL;
+		list[0].cmd[0] = "/bin/echo";
+		list[0].cmd[1] = "0123";
+		list[0].cmd[2] = NULL;
 		list[0].next = &list[1];
 		list[1].cmd[0] = "/usr/bin/wc";
 		list[1].cmd[1] = NULL;
