02c1f1f4d2e343e01c707ef8741987df3992380d
Anas TABITI <tabiti.anas@ofppt-edu.ma>
2022-06-01 13:45:17 +0100

    d

--- exec/ft_others.c
+++ exec/ft_others.c
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 10:24:12 by atabiti           #+#    #+#             */
-/*   Updated: 2022/06/01 13:31:46 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/06/01 13:44:42 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,84 +18,104 @@ int ft_bin_usr_sbin(struct s_list *list)
 	char *userbin;
 	char *sbin;
 	sbin = ft_strjoin("/sbin/", list->input);
-	bin = ft_strjoin("/bin/", list->input);
 	userbin = ft_strjoin("/usr/bin/", list->input);
 	char path[PATH_MAX];
 	char *main_path;
 	char *pwd = ft_strjoin(getcwd(path, PATH_MAX), "/");
 	main_path = ft_strjoin(pwd, list->input);
-	if (ft_strncmp("123", list->input, 3) == 0)
-	{
-		///
-		int x = 0;
-		int lenght = ft_strlen("PATH=");
-		while (!(ft_strnstr(list->environ[x], "PATH=", lenght)))
-		{
-			x++;
-			if (list->environ[x] == NULL)
-				return 0;
-		}
+	int i;
+	///
+	char **new;
 
-		///
-		char **new = ft_split(list->environ[x], ':');
-		int i = 0;
-		while (new[i])
-		{
-			printf("%s      \n", new[i]);
-			i++;
-		}
-	}
-	if (access(bin, F_OK) == 0)
+	///
+	int x = 0;
+	int lenght = ft_strlen("PATH=");
+	while (!(ft_strnstr(list->environ[x], "PATH=", lenght)))
 	{
-		char *cmd[] = {list->input, NULL};
-		int idd = fork();
-		if (idd == 0)
-		{
-			execve(bin, cmd, NULL);
-		}
-		// waitpid(idd, 0, 0);
-		wait(0);
-	}
-	else if (access(userbin, F_OK) == 0)
-	{
-		char *cmd[] = {list->input, NULL};
-		int gg = fork();
-		if (gg == 0)
-		{
-			execve(userbin, cmd, NULL);
-		}
-		// waitpid(gg, 0, 0);
-		wait(0);
-	}
-	else if (access(list->input, F_OK) == 0)
-	{
-		char *cmd[] = {list->input, NULL};
-		int gg = fork();
-		if (gg == 0)
-		{
-			execve(list->input, cmd, NULL);
-		}
-		// waitpid(gg, 0, 0);
-		wait(0);
-	}
-	else if (access(main_path, F_OK) == 0)
-	{
-		char *cmd[] = {list->input, NULL};
-		int gg = fork();
-		if (gg == 0)
-		{
-			execve(main_path, cmd, NULL);
-		}
-		wait(0);
-	}
-	else if (access(sbin, F_OK) == 0)
-	{
-		if (fork() == 0)
-		{
-			execve(sbin, NULL, NULL);
-		}
-		wait(0);
+		x++;
+		if (list->environ[x] == NULL)
+			return 0;
 	}
 
-	return 0;
+	///
+	new = ft_split(list->environ[x], ':');
+	i = 0;
+	while (new[i])
+	{
+		// printf("%s\n", new[i]);
+		i++;
+	}
+
+	i = 0;
+	while (!new[i++])
+	{
+		bin = ft_strjoin(new[i], list->input);
+		if (access(bin, F_OK) == 0)
+		{
+			if (fork() == 0)
+			{
+
+				char *cmd[] = {list->input, NULL};
+				execve(bin, cmd, NULL);
+			}
+			else
+			{
+				wait(0);
+			}
+		}
+	}
+
+// if (access(bin, F_OK) == 0)
+// {
+// 	char *cmd[] = {list->input, NULL};
+// 	int idd = fork();
+// 	if (idd == 0)
+// 	{
+// 		execve(bin, cmd, NULL);
+// 	}
+// 	// waitpid(idd, 0, 0);
+// 	wait(0);
+// }
+// else if (access(userbin, F_OK) == 0)
+// {
+// 	char *cmd[] = {list->input, NULL};
+// 	int gg = fork();
+// 	if (gg == 0)
+// 	{
+// 		execve(userbin, cmd, NULL);
+// 	}
+// 	// waitpid(gg, 0, 0);
+// 	wait(0);
+// }
+// else if (access(list->input, F_OK) == 0)
+// {
+// 	char *cmd[] = {list->input, NULL};
+// 	int gg = fork();
+// 	if (gg == 0)
+// 	{
+// 		execve(list->input, cmd, NULL);
+// 	}
+// 	// waitpid(gg, 0, 0);
+// 	wait(0);
+// }
+// else if (access(main_path, F_OK) == 0)
+// {
+// 	char *cmd[] = {list->input, NULL};
+// 	int gg = fork();
+// 	if (gg == 0)
+// 	{
+// 		execve(main_path, cmd, NULL);
+// 	}
+// 	wait(0);
+// }
+// else if (access(sbin, F_OK) == 0)
+// {
+// 	if (fork() == 0)
+// 	{
+// 		execve(sbin, NULL, NULL);
+// 	}
+// 	wait(0);
+// }
+
+return 0;
 }
