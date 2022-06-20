d5ba49b52833606691c3704ac0f2aeaaa0db99eb
anastabiti <anastabiti@gmail.com>
2022-06-20 07:34:48 +0100

    fixing

--- exec/ft_here_doc.c
+++ exec/ft_here_doc.c
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 07:14:58 by atabiti           #+#    #+#             */
-/*   Updated: 2022/06/20 07:28:27 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/06/20 07:34:13 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,20 @@ int here_doc(struct s_list *list)
 {
     int  i = 0;
     while(i < list->cmd_nbr)
-    {
+    { 
 
         if(ft_strncmp(HEREDOC , list[i].type[i], 7) == 0)
         { char *line;
+         int fd;
             while(1)
             {
-                signal(SIGINT, handler);
-                signal(SIGQUIT, SIG_IGN);
-                
+               
               
                 if(fork() == 0)
-                {
-                    int fd = open("HEREDOCTMP", O_RDWR, 0600);
+                { signal(SIGINT, handler);
+                signal(SIGQUIT, SIG_IGN);
+                
+                     fd = open("HEREDOCTMP", O_RDWR | O_CREAT , 0600);
                     line = readline(">");
                       if(ft_strncmp(line , list[i].delimiter[i], 3) == 0)
                 {
@@ -37,10 +38,13 @@ int here_doc(struct s_list *list)
                 }
                 write(fd, line, ft_strlen(line));
                 write(fd, "\n", 1);
+                free(line);
                 }
                 else
                 {
                     wait(NULL);
+                    close(fd);
+                    exit(1);
                 }
             }
         }
