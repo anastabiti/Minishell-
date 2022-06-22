74b344a59e1645cf553a5acba0e1e633a82fe7a8
anastabiti <anastabiti@gmail.com>
2022-06-22 20:40:26 +0100

    adding ./program and /path/path/program in testing mod

--- test.c
+++ test.c
@@ -1,15 +1,26 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
-int	main(void)
+int	main(int ac, char **av)
 {
+    
 	///home/anas/hadahowalkhar/last2206/.minishell it must not work
 	if (fork() == 0)
 	{
-		char *cmd[11] = {"minishell", NULL};
-		execve("/home/anas/hadahowalkhar/last2206/minishell", cmd, NULL);
-		printf("failed");
+		if (av[1][0] == '.' && av[1][1] == '/')
+		{
+			char *cmd[3] = {av[1], NULL};
+			execve(av[1], cmd, NULL);
+			printf("failed");
+		}
+		else if (av[1][0] == '/')
+		{
+			char *cmd[3] = {av[1], NULL};
+			execve(av[1], cmd, NULL);
+		}
+		exit(1);
 	}
 	else
 		wait(NULL);
