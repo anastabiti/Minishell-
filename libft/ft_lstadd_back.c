72bf1caaf6e5f4e44114a48ab71b76453b96d958
Mohamed Karim <karimkar999@gmail.com>
2022-06-25 17:12:33 +0100

    add functions used in linked list

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 14:07:06 by mkarim            #+#    #+#             */
+/*   Updated: 2021/11/12 10:23:12 by mkarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*last;
+
+	last = *alst;
+	if (new == NULL)
+		return ;
+	if (last == NULL)
+	{
+		*alst = new;
+		return ;
+	}
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = new;
+}
---
+++ libft/ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 14:08:42 by mkarim            #+#    #+#             */
+/*   Updated: 2021/11/12 11:37:27 by mkarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*last;
+
+	last = *lst;
+	if (lst == NULL)
+		return ;
+	while (*lst != NULL)
+	{
+		last = *lst;
+		del(last->content);
+		free(last);
+		(*lst) = (*lst)->next;
+	}
+}
