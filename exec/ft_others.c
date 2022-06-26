4b8af702b84c0ee72fb9a0590d5f003b149446d5
anastabiti <anastabiti@gmail.com>
2022-06-26 10:15:53 +0100

    i forgot to pase env to other programs

--- exec/ft_others.c
+++ exec/ft_others.c
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 10:24:12 by atabiti           #+#    #+#             */
-/*   Updated: 2022/06/26 10:09:20 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/06/26 10:13:54 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int	ft_check_programs(t_cmdl *list)
 		&& list[list->cmd_iteration].cmd[1] == '/')
 	{
 		argv = create_argv_for_execve(list);
-		execve(list[list->cmd_iteration].cmd, argv, NULL);
+		execve(list[list->cmd_iteration].cmd, argv, list->environ);
 		printf("Minishell : %s : No such file or directory\n",
 				list[list->cmd_iteration].cmd);
 		exit(127);
@@ -59,7 +59,7 @@ int	ft_check_programs(t_cmdl *list)
 	if (list[list->cmd_iteration].cmd[0] == '/')
 	{
 		argv = create_argv_for_execve(list);
-		execve(list[list->cmd_iteration].cmd, argv, NULL);
+		execve(list[list->cmd_iteration].cmd, argv, list->environ);
 		printf("Minishell : %s : No such file or directory\n",
 				list[list->cmd_iteration].cmd);
 		exit(127);
