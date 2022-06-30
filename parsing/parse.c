18017882dd7c60c4eb768065da736d28f112c13c
Mohamed Karim <mkarim@e3r6p14.1337.ma>
2022-06-30 01:01:08 +0100

    starting another way for parsing

--- parsing/parse.c
+++ parsing/parse.c
@@ -12,36 +12,70 @@
 
 #include "parse.h"
 
+int	get_num_cmd(t_data *data)
+{
+	int	i;
+	int	quotes[2];
+	int	num_cmd;
+
+	i = -1;
+	num_cmd = 1;
+	quotes[0] = 1;
+	quotes[1] = 1;
+	while (data->cmd_line[++i])
+	{
+		if (data->cmd_line[i] == '\"' && quotes[1] == 1)
+			quotes[0] *= -1;
+		else if (data->cmd_line[i] == '\'' && quotes[0] == 1)
+			quotes[1] *= -1;
+		if (quotes[1] + quotes[0] == 2)
+			if (data->cmd_line[i] == '|')
+				num_cmd++;
+	}
+	return (num_cmd);
+}
+
+int	get_min_cmd(t_data *data)
+{
+	int	i;
+	int	quotes[2];
+	int	index;
+	int	j;
+
+	i = -1;
+	j = 0;
+	quotes[0] = 1;
+	quotes[1] = 1;
+	index = 0;
+	data->num_cmd = get_num_cmd(data);
+	return (0);
+}
+
+void	prep_token(t_data *data)
+{
+	if (!get_min_cmd(data))
+		return ;
+}
+
 int	main(int argc, char **argv)
 {
-	char	*line;
-	char	**split;
-	t_cmdl	*cmd;
-	int		i;
+	t_data	data;
 
 	if (argc == 1)
 	{
 		while (1)
 		{
-			line = readline(PROMPT);
-			// printf("%s\n", line);
-			add_history(line);
-			if (!check_err_pipe(line))
-				continue;
-			split = ft_split(line, '|');
-			i = -1;
-			while (split[++i])
+			data.cmd_line = readline(PROMPT);
+			if (!data.cmd_line)
 			{
-				split[i] = rm_spaces(split[i]);
-				// printf("%s\n", split[i]);
+				// ft_putendl_fd("exit");
+				// exit(0);
 			}
-			cmd->pipe = i;
-			i = -1;
-			while (split[++i])
+			if (ft_strlen(data.cmd_line))
 			{
-				ft_split_cmd(split[i]);
+				add_history(data.cmd_line);
+				prep_token(&data);
 			}
-			// printf("%d\n", i);
 		}
 	}
 }
