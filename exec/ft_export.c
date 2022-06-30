ca97953ff3126e6bcb15cecda785db1bca1aa495
anastabiti <anastabiti@gmail.com>
2022-06-30 11:43:55 +0100

    sort the env variables (export without args)

--- exec/ft_export.c
+++ exec/ft_export.c
@@ -11,38 +11,83 @@
 /* ************************************************************************** */
 
 #include "../minishell.h"
+int	ft_sort_env(struct s_envp *envp, t_cmdl *cmd)
+{
+	int		i;
+	int		j;
+	int		count;
+	char	*tmp ;
+	char *tmp1;
+	char	**joined;
+	joined = malloc(sizeof(char **) * 101);
+	i = 0;
+	j = 0;
+	while (i < envp->envpitems)
+	{
+		tmp1 = ft_strjoin( envp->name[i], "=\"");
+		joined[i] = ft_strjoin(tmp1, envp->value[i]);
+					printf(" %s \n\n", joined[i]);
 
+		i++;
+	}
+	// i = 0;
+	// while (i < envp->envpitems)
+	// {
+	// 	j = i + 1;
+	// 	while (j < envp->envpitems)
+	// 	{
+	// 		tmp = malloc(sizeof(char *) * ft_strlen(joined[i]));
+	// 		if (strcmp(joined[i],joined[j]) > 0)
+	// 		{
+	// 			memmove (tmp,joined[i], ft_strlen(joined[i]) + 1);
+	// 			memmove (joined[i], joined[j], ft_strlen(joined[j]) + 1);
+	// 			memmove (joined[j], tmp, ft_strlen(tmp) + 1);
+
+	// 		}
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
+	// i = 0;
+	// while (i < envp->envpitems)
+	// {
+	// 		printf("declare -x %s=\"\n", joined[i]);
+	// 		i++;
+	// }
+	return (0);
+}
 int	ft_export_1(struct s_envp *envp, t_cmdl *cmd)
 {
 	int		i;
 	char	**new;
+	char	*clone;
 	int		x;
 
 	x = 0;
 	if (cmd->args[0] == NULL)
 	{
 		i = 0;
-		while (x < envp->envpitems)
-		{
-
-		// 	new = ft_split(envp->environment[x], '=');
-		// 	while (new[i])
-		// 	{
-				// ft_putstr_fd("declare -x ", 1);
-			printf("declare -x %s=\"%s\"\n", envp->name[x], envp->value[x]);
-		// 		ft_putstr_fd(new[i], 1);
-		// 		write(1, "=\"", 2);
-		// 		while (new[i + 1] != NULL)
-		// 		{
-		// 			ft_putstr_fd(new[i + 1], 1);
-		// 			i++;
-		// 		}
-		// 		write(1, "\"\n", 2);
-		// 		i++;
-		// 	}
-			x++;
-			// i = 0;
-		}
+		ft_sort_env(envp, cmd);
+		// while (x < envp->envpitems)
+		// {
+		// // 	// 	new = ft_split(envp->environment[x], '=');
+		// // 	// 	while (new[i])
+		// // 	// 	{
+		// // 	// ft_putstr_fd("declare -x ", 1);
+		// 	// printf("declare -x %s=\"%s\"\n", envp->name[x], envp->value[x]);
+		// // 	// 		ft_putstr_fd(new[i], 1);
+		// // 	// 		write(1, "=\"", 2);
+		// // 	// 		while (new[i + 1] != NULL)
+		// // 	// 		{
+		// // 	// 			ft_putstr_fd(new[i + 1], 1);
+		// // 	// 			i++;
+		// // 	// 		}
+		// // 	// 		write(1, "\"\n", 2);
+		// // 	// 		i++;
+		// // 	// 	}
+		// 	x++;
+		// 	// i = 0;
+		// }
 		return (0);
 	}
 	return (1);
@@ -60,22 +105,22 @@ int	ft_export(struct s_envp *envp, t_cmdl *cmd, int i)
 
 	split = ft_split(cmd[0].args[i], '=');
 	to_be_exported = split[0];
-	
-		if (ft_is_alpha(split[0][0]) == 0)
-		{
-			printf("Minishell: export: `%s': not a valid identifier\n",cmd[0].args[i]);
-			return (1);
-		}
-	
+	if (ft_is_alpha(split[0][0]) == 0)
+	{
+		printf("Minishell: export: `%s': not a valid identifier\n",
+				cmd[0].args[i]);
+		return (1);
+	}
 	x = 0;
 	// printf("to_be_exported is %s  \n", split[0]);
 	len = ft_strlen(split[0]);
 	x = 0;
 	while (x < envp->envpitems)
 	{
-		if (ft_strnstr(envp->environment[x], to_be_exported, len))
+		if (ft_strncmp(envp->name[x], to_be_exported, len))
 		{
-			envp->environment[x] = cmd[0].args[i];
+			if (split[1])
+				envp->environment[x] = cmd[0].args[i];
 			return (0);
 		}
 		x++;
@@ -101,6 +146,6 @@ int	ft_export(struct s_envp *envp, t_cmdl *cmd, int i)
 	envp->envpitems++;
 	// free(envp->environment);
 	envp->environment = new;
-	ft_split_env(envp,  envp->environment);
+	ft_split_env(envp, envp->environment);
 	return (0);
 }
--- exec/ft_unset.c
+++ exec/ft_unset.c
@@ -17,14 +17,13 @@ int	ft_search_for_variable_in_env(struct s_envp *envp, t_cmdl *list, int arg)
 	int	x;
 	int	i;
 
-	lenght = ft_strlen(list[list->cmd_iteration].args[arg]);
+	lenght = ft_strlen(list[list->cmd_iteration].args[0]);
 	x = 0;
 	i = 0;
 	while (x < envp->envpitems)
 	{
-		list->envsplited = ft_split(envp->environment[x], '=');
-		if (ft_strncmp(list->envsplited[0], list[list->cmd_iteration].args[arg],
-				lenght) == 0)
+		// list->envsplited = ft_split(envp->environment[x], '=');
+		if (ft_strncmp(envp->name[x], list[list->cmd_iteration].args[0], lenght) == 0)
 		{
 			list->envvarpos = x;
 			return (1);
